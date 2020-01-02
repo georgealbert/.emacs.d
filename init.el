5ac7aba848a73409c50bec109058686c5eff0f42
Albert <georgealbert@qq.com>
2020-01-02 17:11:31 +0800

    参考doom-emacs增加gcmh-mode处理gc，改得乱七八糟

--- init.el
+++ init.el
@@ -2,121 +2,41 @@
 
 ;; This sets up the load path so that we can override it
 
-;; 启动优化
-(defvar doom-gc-cons-threshold 16777216 ; 16mb
-  "The default value to use for `gc-cons-threshold'. If you experience freezing,
-decrease this. If you experience stuttering, increase this.")
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
 
-(defvar doom-gc-cons-upper-limit 536870912 ; 512mb
-  "The temporary value for `gc-cons-threshold' to defer it.")
+;; [2018-11-30 周五 11:45:50] 这个函数是必须的，否则启动报错。
+(package-initialize nil)
 
-(defvar doom--file-name-handler-alist file-name-handler-alist)
+;; [2019-10-30 周三 17:37:50] emacs 27.0不需要了
+;; (setq package-enable-at-startup nil)
 
-(defun doom|restore-startup-optimizations ()
-  "Resets garbage collection settings to reasonable defaults (a large
-`gc-cons-threshold' can cause random freezes otherwise) and resets
-`file-name-handler-alist'."
-  (setq file-name-handler-alist doom--file-name-handler-alist)
-  ;; Do this on idle timer to defer a possible GC pause that could result; also
-  ;; allows deferred packages to take advantage of these optimizations.
-  (run-with-idle-timer
-   3 nil
-   (lambda ()
-     (setq-default gc-cons-threshold doom-gc-cons-threshold)
-     ;; To speed up minibuffer commands (like helm and ivy), we defer garbage
-     ;; collection while the minibuffer is active.
-     (defun doom|defer-garbage-collection ()
-       (setq gc-cons-threshold doom-gc-cons-upper-limit))
-     (defun doom|restore-garbage-collection ()
-       ;; Defer it so that commands launched from the minibuffer can enjoy the
-       ;; benefits.
-       (run-at-time 1 nil (lambda () (setq gc-cons-threshold doom-gc-cons-threshold))))
-     (add-hook 'minibuffer-setup-hook #'doom|defer-garbage-collection)
-     (add-hook 'minibuffer-exit-hook  #'doom|restore-garbage-collection)
-     ;; GC all sneaky breeky like
-     (add-hook 'focus-out-hook #'garbage-collect))))
+(require 'benchmark-init-modes)
+(require 'benchmark-init)
+(benchmark-init/activate)
 
-;; 显示加载时间
-(defvar albert-init-time 'nil
-  "The time it took, in seconds, for Doom Emacs to initialize.")
+;; (setq org-modules
+(defvar org-modules
+  '(;; org-w3m
+    ;; org-bbdb
+    ;; org-bibtex
+    ;; org-docview
+    ;; org-gnus
+    ;; org-info
+    ;; org-irc
+    ;; org-mhe
+    ;; org-rmail
+    ))
 
-(defun albert|display-benchmark()
-  "Display a benchmark, showing number of packages and modules, and how quickly
-they were loaded at startup."
-  (message "Emacs loaded %s packages in %.03fs"
-           (length package-activated-list)
-           (or albert-init-time
-               (setq albert-init-time
-                     (float-time (time-subtract (current-time) before-init-time))))))
+;; (require 'org)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/Albert.org"))
+;; (org-babel-load-file (expand-file-name "~/.emacs.d/Albert_org_config.org"))
 
-(if (ignore-errors (or after-init-time noninteractive))
-    (setq gc-cons-threshold doom-gc-cons-threshold)
-  ;; A big contributor to startup times is garbage collection. We up the gc
-  ;; threshold to temporarily prevent it from running, then reset it later in
-  ;; `doom|restore-startup-optimizations'.
-  (setq gc-cons-threshold doom-gc-cons-upper-limit)
-  ;; This is consulted on every `require', `load' and various path/io functions.
-  ;; You get a minor speed up by nooping this.
-  (setq file-name-handler-alist nil)
-  ;; Not restoring these to their defaults will cause stuttering/freezes.
-
-
-  ;; (let (
-  ;;       ;; 加载的时候临时增大`gc-cons-threshold'以加速启动速度。
-  ;;       ;; (gc-cons-threshold most-positive-fixnum)
-  ;;       (gc-cons-threshold 536870912) ;; 512mb
-  ;;       ;; 清空避免加载远程文件的时候分析文件。
-  ;;       (file-name-handler-alist nil))
-
-  (require 'benchmark-init-modes)
-  (require 'benchmark-init)
-  (benchmark-init/activate)
-
-  ;; 下面才写你的其它配置
-
-  ;; doom-emacs用的hook是 window-setup-hook
-  (add-hook 'window-setup-hook #'albert|display-benchmark)
-
-  ;; [2018-11-29 周四 22:28:22] 测试emacs启动需要30s+的问题
-  ;; (toggle-debug-on-error)
-
-  ;; [2018-11-30 周五 11:45:50] 这个函数是必须的，否则启动报错。
-  (package-initialize nil)
-
-  ;; Override the packages with the git version of Org and other packages
-  ;; (add-to-list 'load-path "~/.emacs.d/lisp/org-8.2.7c/lisp")
-  ;; (add-to-list 'load-path "~/.emacs.d/lisp/org-8.2.7c/contrib/lisp")
-
-  ;; Load the rest of the packages
-  ;; (package-initialize t)
-
-  ;; [2019-10-30 周三 17:37:50] emacs 27.0不需要了
-  (setq package-enable-at-startup nil)
-
-  ;; (setq org-modules
-  (defvar org-modules
-    '(;; org-w3m
-      ;; org-bbdb
-      ;; org-bibtex
-      ;; org-docview
-      ;; org-gnus
-      ;; org-info
-      ;; org-irc
-      ;; org-mhe
-      ;; org-rmail
-      ))
-
-  ;; (require 'org)
-  (require 'ob-tangle)
-  (org-babel-load-file (expand-file-name "~/.emacs.d/Albert.org"))
-  ;; (org-babel-load-file (expand-file-name "~/.emacs.d/Albert_org_config.org"))
-
-  ;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/my_elisp"))
-  ;; (require 'init-albert)
-
-  ;; )
-
-  (add-hook 'after-init-hook #'doom|restore-startup-optimizations))
+;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/my_elisp"))
+;; (require 'init-albert)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -132,7 +52,7 @@ they were loaded at startup."
  '(jdee-db-spec-breakpoint-face-colors (cons "#100e23" "#7f7f7f"))
  '(objed-cursor-color "#FF0000")
  '(package-selected-packages
-   '(lua-mode org-download org-static-blog lsp-python-ms winum treemacs-evil flycheck-posframe dired-k neotree all-the-icons doom-modeline doom-themes py-autopep8 helm-ag helm-gtags ggtags go-mode xah-find window-numbering web-mode use-package spinner sesman seq queue powerline pkg-info paren-face org2blog markdown-mode magit highlight-parentheses helm-swoop evil-paredit elpy dired+ diminish benchmark-init))
+   '(gcmh vimrc-mode eglot lua-mode org-download org-static-blog lsp-python-ms winum treemacs-evil flycheck-posframe dired-k neotree all-the-icons doom-modeline doom-themes py-autopep8 helm-ag helm-gtags ggtags go-mode xah-find window-numbering web-mode use-package spinner sesman seq queue powerline pkg-info paren-face org2blog markdown-mode magit highlight-parentheses helm-swoop evil-paredit elpy dired+ diminish benchmark-init))
  '(safe-local-variable-values '((encoding . UTF-8)))
  '(send-mail-function 'mailclient-send-it)
  '(vc-annotate-background "#181a26")
