b41388527d8fa33861e2c5be060af9e1559e6ca9
Albert <georgealbert@qq.com>
2020-02-20 23:33:26 +0800

    add pdump

--- init.el
+++ init.el
@@ -1,7 +1,36 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+(defvar albert-dumped nil
+  "non-nil when a dump file is loaded (because dump.el sets this variable).")
+
+(defmacro albert-if-dump (then &rest else)
+  "Evaluate IF if running with a dump file, else evaluate ELSE."
+  (declare (indent 1))
+  `(if albert-dumped
+       ,then
+     ,@else))
+
+(albert-if-dump
+    (progn
+      (message "using pdump...")
+      (setq load-path albert-dumped-load-path)
+      (global-undo-tree-mode -1)
+      (global-font-lock-mode)
+      (add-hook 'after-init-hook
+                (lambda ()
+                  (save-excursion
+                    (switch-to-buffer "*scratch*")
+                    (fundamental-mode)))))
+  ;; add load-path’s and load autoload files
+  (package-initialize))
+
+;; load theme
+(when window-system
+  (albert-if-dump
+      (enable-theme 'doom-deeper-blue)))
+
 ;; [2018-11-30 周五 11:45:50] 这个函数是必须的，否则启动报错。
-(package-initialize nil)
+;; (package-initialize nil)
 
 ;; [2019-10-30 Wed 17:34:45] 27.0 does not need this
 (setq package-enable-at-startup nil)
@@ -9,6 +38,9 @@
 (require 'benchmark-init-modes)
 (require 'benchmark-init)
 (benchmark-init/activate)
+
+;; (require 'esup)
+;; (autoload 'esup "esup" "Emacs Start Up Profiler." nil)
 
 (defvar org-modules
   '())
@@ -24,7 +56,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes '(default))
  '(package-selected-packages
-   '(gcmh treemacs treemacs-evil treemacs-projectile treemacs-icons-dired lsp-python-ms ccls flycheck-posframe dired-k helm-ag doom-modeline doom-themes py-autopep8 flycheck pyim ggtags go-mode xah-find window-numbering web-mode use-package spinner sesman seq queue pkg-info paren-face org2blog markdown-mode magit highlight-parentheses helm-swoop evil-paredit elpy diminish benchmark-init)))
+   '(ace-window gcmh treemacs treemacs-evil treemacs-projectile treemacs-icons-dired lsp-python-ms ccls flycheck-posframe dired-k helm-ag doom-modeline doom-themes py-autopep8 flycheck pyim ggtags go-mode web-mode use-package spinner sesman seq queue pkg-info paren-face org2blog markdown-mode magit highlight-parentheses helm-swoop evil-paredit benchmark-init)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
