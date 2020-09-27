4df2cf55bbdc6c0a51cc4c85892e431998fb2e59
Albert <georgealbert@qq.com>
2020-09-27 11:16:18 +0800

    注释掉benchmark-init，初始化.emacs.d会因为没有benchmark-init启动报错

--- init.el
+++ init.el
@@ -38,9 +38,9 @@
 ;; [2019-10-30 周三 17:37:50] emacs 27.0不需要了
 ;; (setq package-enable-at-startup nil)
 
-(require 'benchmark-init-modes)
-(require 'benchmark-init)
-(benchmark-init/activate)
+;; (require 'benchmark-init-modes)
+;; (require 'benchmark-init)
+;; (benchmark-init/activate)
 
 ;; (defvar org-modules
 ;;   '(;; org-w3m
@@ -108,7 +108,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#100e23" "#7f7f7f"))
  '(objed-cursor-color "#FF0000")
  '(package-selected-packages
-   '(hydra wgrep pyim find-file-in-project pinyinlib ivy-hydra ivy-prescient prescient amx smartparens cmuscheme hl-todo gcmh vimrc-mode lua-mode org-download lsp-python-ms winum treemacs-evil flycheck-posframe dired-k all-the-icons doom-modeline doom-themes py-autopep8 helm-ag ggtags go-mode web-mode use-package spinner sesman seq queue pkg-info paren-face markdown-mode magit highlight-parentheses helm-swoop evil-paredit benchmark-init))
+   '(lsp-pyright hydra wgrep pyim find-file-in-project pinyinlib ivy-hydra ivy-prescient prescient amx smartparens cmuscheme hl-todo gcmh vimrc-mode lua-mode org-download lsp-python-ms winum treemacs-evil flycheck-posframe dired-k all-the-icons doom-modeline doom-themes py-autopep8 helm-ag ggtags go-mode web-mode use-package spinner sesman seq queue pkg-info paren-face markdown-mode magit highlight-parentheses helm-swoop evil-paredit benchmark-init))
  '(safe-local-variable-values '((encoding . UTF-8)))
  '(send-mail-function 'mailclient-send-it)
  '(vc-annotate-background "#181a26")
