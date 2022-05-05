c0afd481730076c4043a703a65ed98522a6673df
albert <georgealbert@qq.com>
2022-05-05 14:24:24 +0800

    * init-tree-sitter.el: 修改vue的编译方式，调整include的路径

--- lisp/init-tree-sitter.el
+++ lisp/init-tree-sitter.el
@@ -21,7 +21,7 @@
   ;; Add Vue for tree-sitter:
   ;;
   ;; 1. git clone git@github.com:ikatyang/tree-sitter-vue.git
-  ;; 2. g++ ./src/parser.c ./src/scanner.cc -fPIC -I /Users/albert/workspace/tree-sitter-vue/ -I /Users/albert/workspace/tree-sitter-vue/src -v --shared -o vue.dylib
+  ;; 2. g++ ./src/parser.c ./src/scanner.cc -fPIC -I./ -I./src -v --shared -o vue.dylib
   ;; 3. cp -p ./vue.dylib ~/.emacs.d/elpa/tree-sitter-langs-*/bin (~/.tree-sitter-langs/bin is path of your tree-sitter-langs repo)
   (tree-sitter-load 'vue)
   (add-to-list 'tree-sitter-major-mode-language-alist '(web-mode . vue))
