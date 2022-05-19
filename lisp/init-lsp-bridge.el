3b0b965eb0a8bd597d2948ba0518fdc785c2703c
albert <georgealbert@qq.com>
2022-05-19 17:56:17 +0800

    * init-lsp-bridge.el: 根据最新的lsp-bridge修改配置

--- lisp/init-lsp-bridge.el
+++ lisp/init-lsp-bridge.el
@@ -4,13 +4,21 @@
   :ensure nil
   :load-path "~/.emacs.d/site-lisp/extensions/lsp-bridge"
   :hook ((python-mode sh-mode lua-mode) . (lambda ()
-                         (require 'lsp-bridge-orderless) ;; make lsp-bridge support fuzzy match, optional
-                         (require 'lsp-bridge-icon) ;; show icon for completion items, optional
-                         (setq-local corfu-auto nil)
-                         (lsp-bridge-mode 1)
-                         ))
+                                            (require 'lsp-bridge-orderless) ;; make lsp-bridge support fuzzy match, optional
+                                            (require 'lsp-bridge-icon) ;; show icon for completion items, optional
+                                            (setq-local corfu-auto nil)
+                                            (lsp-bridge-mode 1)))
   :config
   (setq lsp-bridge-python-command "/Users/albert/.virtualenvs/pandas/bin/python")
+
+  (setq lsp-bridge-completion-provider 'corfu)
+
+  ;; (require 'corfu)
+  ;; (require 'corfu-info)
+  ;; (require 'corfu-history)
+  ;; (global-corfu-mode)               ;; use corfu as completion ui
+  ;; (corfu-history-mode t)
+  ;; (global-lsp-bridge-mode)
 
   ;; (setq lsp-bridge-enable-log t)
   ;; (dolist (hook (list
