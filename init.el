b9f4acfcec6784f8d13e7656c6ee112f50e77fa9
albert <georgealbert@qq.com>
2022-05-23 16:23:36 +0800

    * init.el: custom variable保存到 custom.el

--- init.el
+++ init.el
@@ -90,50 +90,5 @@
 (if (or (eq system-type 'windows-nt) (eq system-type 'darwin) (eq window-system 'x))
     (toggle-frame-maximized))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#181a26" "#FF0000" "#4eee94" "#FFFF00" "#0000cd" "#f08080" "#00FFFF" "#cccccc"])
- '(custom-safe-themes '(default))
- '(fci-rule-color "#ffe4b5")
- '(jdee-db-active-breakpoint-face-colors (cons "#100e23" "#EE82EE"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#100e23" "#4eee94"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#100e23" "#7f7f7f"))
- '(objed-cursor-color "#FF0000")
- '(package-selected-packages
-   '(tree-sitter-langs dirvish all-the-icons-ivy-rich swift-mode org-static-blog yaml-mode vue-mode add-node-modules-path lsp-pyright hydra wgrep find-file-in-project pinyinlib ivy-hydra ivy-prescient prescient amx cmuscheme hl-todo gcmh vimrc-mode lua-mode org-download winum treemacs-evil flycheck-posframe all-the-icons doom-modeline doom-themes py-autopep8 ggtags go-mode web-mode use-package spinner sesman seq queue pkg-info markdown-mode magit highlight-parentheses evil-paredit benchmark-init))
- '(safe-local-variable-values '((encoding . UTF-8)))
- '(send-mail-function 'mailclient-send-it)
- '(vc-annotate-background "#181a26")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#4eee94")
-    (cons 40 "#89f362")
-    (cons 60 "#c3f931")
-    (cons 80 "#FFFF00")
-    (cons 100 "#ffe100")
-    (cons 120 "#ffc300")
-    (cons 140 "#FFA500")
-    (cons 160 "#fa982a")
-    (cons 180 "#f58c55")
-    (cons 200 "#f08080")
-    (cons 220 "#f55555")
-    (cons 240 "#fa2a2a")
-    (cons 260 "#FF0000")
-    (cons 280 "#df1f1f")
-    (cons 300 "#bf3f3f")
-    (cons 320 "#9f5f5f")
-    (cons 340 "#ffe4b5")
-    (cons 360 "#ffe4b5")))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ivy-minibuffer-match-face-1 ((t (:inherit font-lock-doc-face :foreground nil))))
- '(lsp-ui-doc-background ((t (:background nil))))
- '(org-mode-line-clock ((t (:background nil :foreground "red" :box (:line-width -1 :style released-button)))) t))
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file 'no-error 'no-message)
