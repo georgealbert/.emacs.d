c91841593354c28168f1bbda9d257717ddbb75d6
albert <georgealbert@qq.com>
2022-07-01 16:37:41 +0800

    * init-lsp-bridge.el: 删除不用的代码

--- lisp/init-lsp-bridge.el
+++ lisp/init-lsp-bridge.el
@@ -10,25 +10,11 @@
               ("C-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
               )
   :load-path "~/.emacs.d/site-lisp/extensions/lsp-bridge"
-  ;; :hook ((python-mode sh-mode lua-mode) . (lambda ()
-  ;;                                           ;; (require 'lsp-bridge-icon) ;; show icon for completion items, optional
-  ;;                                           ;; (require 'lsp-bridge-orderless) ;; make lsp-bridge support fuzzy match, optional
-  ;;                                           ;; (setq-local corfu-auto nil)
-  ;;                                           (lsp-bridge-mode 1)
-  ;;                                           ))
   :config
   (setq lsp-bridge-default-mode-hooks
         (remove 'org-mode-hook lsp-bridge-default-mode-hooks))
 
   (setq lsp-bridge-python-command "/Users/albert/.virtualenvs/pandas/bin/python")
-
-  ;; (setq lsp-bridge-completion-provider 'corfu)
-
-  ;; (require 'corfu)
-  ;; (require 'corfu-info)
-  ;; (require 'corfu-history)
-  ;; (global-corfu-mode)               ;; use corfu as completion ui
-  ;; (corfu-history-mode t)
 
   (global-lsp-bridge-mode)
 
