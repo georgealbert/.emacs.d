cba195b2786c3a03df36ceb145421b5c90ce3453
georgealbert <georgealbert@qq.com>
2023-01-28 15:24:57 +0800

    * lisp/init-lsp-bridge.el: 根据最新的lsp-bridge api调整快捷键和tooltip字体大小

--- lisp/init-lsp-bridge.el
+++ lisp/init-lsp-bridge.el
@@ -4,8 +4,8 @@
   :ensure nil
   :defer 2
   :bind (:map lsp-bridge-mode
-              ("C-s-j" . lsp-bridge-jump-to-next-diagnostic) ;显示下一个错误
-              ("C-s-k" . lsp-bridge-jump-to-prev-diagnostic) ;显示上一个错误
+              ("C-s-j" . lsp-bridge-diagnostic-jump-next) ;显示下一个错误
+              ("C-s-k" . lsp-bridge-diagnostic-jump-prev) ;显示上一个错误
               ("C-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
               ("C-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
               )
@@ -14,9 +14,20 @@
   (setq lsp-bridge-default-mode-hooks
         (remove 'org-mode-hook lsp-bridge-default-mode-hooks))
 
-  (setq lsp-bridge-python-command "/Users/albert/.virtualenvs/pandas/bin/python")
+  (setq lsp-bridge-default-mode-hooks
+        (remove 'emacs-lisp-mode-hook lsp-bridge-default-mode-hooks))
+
+  ;; (setq lsp-bridge-python-command "/Users/albert/.virtualenvs/pandas/bin/python")
+  (setq lsp-bridge-python-command "/usr/local/var/pyenv/shims/python")
 
   (global-lsp-bridge-mode)
+
+  ;; tooltip的字体大小从默认的130增加到160
+  (setq lsp-bridge-lookup-doc-tooltip-font-height 160)
+
+  ;; 关闭tempel可以让补全更快些
+  ;; https://emacs-china.org/t/lsp-bridge/20786/2341
+  (setq acm-enable-tempel nil)
 
   ;; (setq lsp-bridge-enable-log t)
   )
