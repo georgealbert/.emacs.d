d4c19c28380cb9d996e2b664d6c90690af91d182
georgealbert <georgealbert@qq.com>
2023-03-03 15:33:40 +0800

    * init-core.el: 增加http proxy的设置

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -11,6 +11,11 @@
 ;;                   elt)
 ;;                ,list)))
 
+;; (setq url-proxy-services
+;;    '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+;;      ("http" . "127.0.0.1:7890")
+;;      ("https" . "127.0.0.1:7890")))
+
 (defmacro add-transient-hook! (hook-or-function &rest forms)
   "Attaches a self-removing function to HOOK-OR-FUNCTION.
 
